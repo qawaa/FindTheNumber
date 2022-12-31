c820c1aa3de6cf46ba8a3ac5ba740e8a1491e852
qawaa <121707411+qawaa@users.noreply.github.com>
2022-12-31 09:46:20 +0300

    FindTheNumber
    FindTheNumber

---
+++ tester/Class1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igra
+{
+    internal class Class1
+    {
+        int userNumber;
+        int _pcNumber;
+        string result;
+        bool correctly;
+
+        public Class1(int pcNumber)
+        {
+            _pcNumber = pcNumber;
+        }
+
+        public Class1 NumberSearch(int userNumber)
+        {
+            this.userNumber = userNumber;
+            correctly = false;
+
+            CreateCompareTwoResult();
+
+            return this;
+        }
+        public string ReturnResult()
+        {
+            return result;
+        }
+        public bool GetSuccess()
+        {
+            return correctly;
+        }
+        private void CreateCompareTwoResult()
+        {
+            if (_pcNumber == userNumber)
+            {
+                result = "Числа равны!";
+                correctly = true;
+            }
+            else
+            {
+                if (userNumber > _pcNumber)
+                {
+                    if (userNumber / _pcNumber >= 10)
+                    {
+                        result = "Много больше!";
+
+                    }
+                    if (userNumber / _pcNumber >= 2 && userNumber / _pcNumber < 10)
+                    {
+                        result = "Больше!";
+                    }
+                    if (userNumber / _pcNumber < 2)
+                    {
+                        result = "Немного больше!";
+                    }
+                }
+                else
+                {
+                    if (_pcNumber / userNumber >= 10)
+                    {
+                        result = "Много меньше!";
+
+                    }
+                    if (_pcNumber / userNumber >= 2 && _pcNumber / userNumber < 10)
+                    {
+                        result = "Меньше!";
+                    }
+                    if (_pcNumber / userNumber < 2)
+                    {
+                        result = "Немного меньше!";
+                    }
+                }
+            }
+        }
+
+    }
+}
+
---
+++ tester/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1;
+            Random random;
+
+            Console.WriteLine("Игра 'Найди число'\nУ вас 10 попыток!\nЗадайте интервал!");
+            while (true)
+            {
+                Console.Write("Введите 1ое число: ");
+                int x = int.Parse(Console.ReadLine()!);
+                Console.Write("Введите 2ое число: ");
+                int y = int.Parse(Console.ReadLine()!);
+
+
+                random = new Random();
+                var Numeric = random.Next(x,y);
+                class1 = new Class1(Numeric);
+                int k = random.Next(x, y);
+                
+
+                Console.WriteLine("Угадайте число в интервале от {0} до {1}\n", x, y);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("Ваше число: ");
+                    if (int.TryParse(Console.ReadLine(), out int input))
+                    {
+                        class1.NumberSearch(input);
+                        Console.WriteLine(class1.ReturnResult());
+                        if (class1.GetSuccess())
+                        {
+                            Console.WriteLine("Правильно!");
+                            break;
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+        
